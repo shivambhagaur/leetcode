9875f53f8cdbf7743487c086d17ac66f2f2f8013
shivambhagaur <shivambhagour8@gmail.com>
2025-08-31 00:56:33 +0530

    Create Q283movezeros.java

---
+++ Q283movezeros.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left =0;
+        int right = nums.length;
+        for(int i =0;i<right;i++){
+            if(nums[i]!=0){
+                nums[left]= nums[i];
+                left++;
+            }
+        }
+
+        while(left<right){
+            nums[left]=0;
+                left++;
+            }
+    }
+}
