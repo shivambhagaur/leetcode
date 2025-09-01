7ac5460ca5abeb35bdb4a78b34977d1cc9d41d5c
shivambhagaur <shivambhagour8@gmail.com>
2025-09-01 19:22:50 +0530

    Create Q525.java

---
+++ Q525.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findMaxLength(int[] nums) {
+        HashMap <Integer,Integer> map = new HashMap<>();
+       map.put(0,-1);
+       int sum =0;
+       int maxlen=0;
+       for(int i =0;i<nums.length;i++){
+        sum+=(nums[i]==0?-1:1);
+        if(map.containsKey(sum)){
+            maxlen = Math.max(maxlen,i-map.get(sum));
+        }
+        else{
+            map.put(sum,i);
+        }
+       }
+       return maxlen;
+    }
+}
